cb0dd02f2e5c03f772a153d5755c44063bb656d4
donovan@ell-ee-dee.local <donovan@ell-ee-dee.local>
2008-06-24 11:54:38 -0700

    Add a description.

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='Spawning',
+    description='Spawning is a wsgi server plugin for paste which uses eventlet to do non-blocking IO '
+        'for http requests and responses. It supports multiple Python processes as well as a threadpool. '
+        'It supports graceful reloading on code change; outstanding requests are given a chance to '
+        'finish while new requests are handled in new Python processes. It plugs into Python Paste.',
     author='Donovan Preston',
     author_email='dsposx@mac.com',
     packages=find_packages(),
